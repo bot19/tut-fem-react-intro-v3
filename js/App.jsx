0b901997db5a525e422f0cf6e9334d21876b80cd
Tony T <sojlit@gmail.com>
2018-08-12 15:19:25 +1000

    build reverted back to use dev server. Server-side rendering files not affecting dev server usage left untouched.

--- js/App.jsx
+++ js/App.jsx
@@ -1,6 +1,6 @@
 // @flow
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import type { Match } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -12,27 +12,29 @@ import preload from "../data.json";
 const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
-	<Provider store={store}>
-		<div className="app">
-			<Switch>
-				<Route exact path="/" component={Landing} />
-				<Route
-					path="/search"
-					component={props => <Search shows={preload.shows} {...props} />}
-				/>
-				<Route
-					path="/details/:id"
-					component={(props: { match: Match }) => {
-						const selectedShow = preload.shows.find(
-							show => props.match.params.id === show.imdbID
-						);
-						return <Details show={selectedShow} {...props} />;
-					}}
-				/>
-				<Route component={FourOhFour} />
-			</Switch>
-		</div>
-	</Provider>
+	<BrowserRouter>
+		<Provider store={store}>
+			<div className="app">
+				<Switch>
+					<Route exact path="/" component={Landing} />
+					<Route
+						path="/search"
+						component={props => <Search shows={preload.shows} {...props} />}
+					/>
+					<Route
+						path="/details/:id"
+						component={(props: { match: Match }) => {
+							const selectedShow = preload.shows.find(
+								show => props.match.params.id === show.imdbID
+							);
+							return <Details show={selectedShow} {...props} />;
+						}}
+					/>
+					<Route component={FourOhFour} />
+				</Switch>
+			</div>
+		</Provider>
+	</BrowserRouter>
 );
 
 export default App;
--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,16 +2,10 @@
 
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const renderApp = () => {
-	render(
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>,
-		document.getElementById("app")
-	);
+	render(<App />, document.getElementById("app"));
 };
 renderApp();
 
